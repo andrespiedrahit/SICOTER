56130b13d043a7992c8db3b31468a0ceea60a674
Victoria1998 <mvlopez63@misena.edu.co>
2017-05-15 10:14:28 -0500

    Se realizo el modulo inicio de sistema de control y control de sistema

---
+++ app.config.js
@@ -0,0 +1,70 @@
+angular.module('CamaraTermicaApp').constant('urlServer', 'http://localhost/CamaraTermica/www/server.php/');
+
+angular.module('CamaraTermicaApp').config(['$routeProvider',
+    function config($routeProvider) {
+        $routeProvider.
+                when('/', {
+                    controller: 'indexController',
+                    templateUrl: 'app/template/index.html'
+                }).
+                when('/login', {
+                    controller: 'loginController',
+                    templateUrl: 'app/template/login.html'
+                }).
+                when('/registrate', {
+                    controller: 'registrateController',
+                    templateUrl: 'app/template/registrate.html'
+                }).
+                when('/modulo', {
+                    controller: 'moduloController',
+                    templateUrl: 'app/template/moduloInteractuar.html'
+                }).
+                when('/temperatura', {
+                    controller: 'temperaturaController',
+                    templateUrl: 'app/template/temperatura.html'
+                }).
+                when('/monitoreo', {
+                    controller: 'monitoreoController',
+                    templateUrl: 'app/template/monitoreo.html'
+                }).
+                when('/graficaTemperatura', {
+                    controller: 'graficaTemperaturaController',
+                    templateUrl: 'app/template/graficaTemperatura.html'
+                }).
+                when('/humedadR', {
+                    controller: 'humedadRController',
+                    templateUrl: 'app/template/humedadR.html'
+                }).
+                when('/graficaHumedadR', {
+                    controller: 'graficaHumedadRController',
+                    templateUrl: 'app/template/graficaHumedadR.html'
+                }).
+                when('/humedadS', {
+                    controller: 'humedadSController',
+                    templateUrl: 'app/template/humedadS.html'
+                }).
+                when('/graficaHumedadS', {
+                    controller: 'graficaHumedadSController',
+                    templateUrl: 'app/template/graficaHumedadS.html'
+                }).
+                when('/consultaG', {
+                    controller: 'consultaGController',
+                    templateUrl: 'app/template/consultaG.html'
+                }).
+                when('/graficaConsultaG', {
+                    controller: 'graficaConsultaGController',
+                    templateUrl: 'app/template/graficaConsultaG.html'
+                }).
+                when('/inicioSistemaControl', {
+                    controller: 'inicioSistemaControlController',
+                    templateUrl: 'app/template/inicioSistemaControl.html'
+                }).
+                when('/sistemaControl', {
+                    controller: 'sistemaControlController',
+                    templateUrl: 'app/template/sistemaControl.html'
+                }).
+                otherwise('/');
+    }
+]);
+
+
